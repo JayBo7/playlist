830aa44ca8285ddc318db53327f194e8d7aa4971
JayBo7 <jvboinapalli@ucdavis.edu>
2018-12-01 17:34:26 -0800

    render bundle using django backend

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+	entry: path.join(__dirname, '/src/client/index.jsx'),
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: [/\.jsx?$/],
+	      exclude: /node_modules/,
+	      use: {
+	      	loader: 'babel-loader',
+	      	options: {
+	      		presets: ['@babel/preset-env', '@babel/preset-react']
+	      	}
+	      }
+			}
+		],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.join(__dirname, 'static/dist')
+	},
+	plugins: [
+        new BundleTracker({filename: './app/webpack-stats.json'}),
+    ],
+}
